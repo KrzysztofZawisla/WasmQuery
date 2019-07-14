c198cdff88d044b2a577a62a578b5e9c64a62074
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-07-14 15:53:25 +0200

    Typos in loop

--- main.go
+++ main.go
@@ -148,14 +148,14 @@ var wasmQueryShowForArray js.Func = js.FuncOf(func(this js.Value, args []js.Valu
 	listOfInlineTags := []string{"B", "BIG", "I", "SMALL", "TT", "ABBR", "ACRONYM", "CITE", "CODE", "DFN", "EM", "KBD", "STRONG", "SAMP", "VAR", "A", "BDO", "BR", "IMG", "MAP", "OBJECT", "Q", "SCRIPT", "SPAN", "SUB", "SUP", "BUTTON", "INPUT", "LABEL", "SELECT", "TEXTAREA"}
 	for i := 0; i < this.Length(); i++ {
 		for j := 0; j < len(listOfInlineTags); j++ {
-			if this.Index(j).Get("tagName").String() == listOfInlineTags[i] {
-				this.Index(j).Get("style").Set("display", "inline")
+			if this.Index(i).Get("tagName").String() == listOfInlineTags[j] {
+				this.Index(i).Get("style").Set("display", "inline")
 			} else {
-				this.Index(j).Get("style").Set("display", "block")
+				this.Index(i).Get("style").Set("display", "block")
 			}
-			valueOfIteration := this.Index(j).Get("style").Get("display")
-			outputArray = append(outputArray, valueOfIteration)
 		}
+		valueOfIteration := this.Index(i).Get("style").Get("display")
+		outputArray = append(outputArray, valueOfIteration)
 	}
 	return outputArray
 })
