4e22c54ed9b258fdffbb8243ef03b7a3ec9e08a1
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-07-14 12:07:59 +0200

    Added return to css function invoked on array of elements

--- main.go
+++ main.go
@@ -67,11 +67,21 @@ var wasmQueryCSSForArray js.Func = js.FuncOf(func(this js.Value, args []js.Value
 		return nil
 	}
 	var selector string = args[0].String()
-	if len(args) >= 2 {
+	var outputArray []interface{}
+	if len(args) == 1 {
+		for i := 0; i < this.Length(); i++ {
+			valueOfIteration := this.Index(i).Get("style").Get(selector)
+			outputArray = append(outputArray, valueOfIteration)
+		}
+		return outputArray
+	} else if len(args) >= 2 {
 		var value string = args[1].String()
 		for i := 0; i < this.Length(); i++ {
 			this.Index(i).Get("style").Set(selector, value)
+			valueOfIteration := this.Index(i).Get("style").Get(selector)
+			outputArray = append(outputArray, valueOfIteration)
 		}
+		return outputArray
 	}
 	return nil
 })
