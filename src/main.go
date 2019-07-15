68b810f179371a2128efaf5e7c1390e85cd67f94
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-07-15 19:15:31 +0200

    Support for array in css function on array of elements and changes in implementation of basic $ function

--- src/main.go
+++ src/main.go
@@ -15,69 +15,25 @@ var wasmQuery js.Func = js.FuncOf(func(this js.Value, args []js.Value) interface
 	if len(args) == 0 {
 		return nil
 	}
-	var domSelector string = args[0].String()
-	if string(domSelector[0]) == "#" || string(domSelector[0]) == "." {
-		var selectorWithoutSymbol string = ""
-		for pos, char := range domSelector {
-			if pos == 0 {
-				continue
-			}
-			selectorWithoutSymbol = selectorWithoutSymbol + string(char)
-		}
-		if string(domSelector[0]) == "#" {
-			returnValue := js.Global().Get("document").Call("getElementById", selectorWithoutSymbol)
-			returnValue.Set("css", wasmQueryCSS)
-			returnValue.Set("hide", wasmQueryHide)
-			returnValue.Set("show", wasmQueryShow)
-			returnValue.Set("showAsBlock", wasmQueryShowAsBlock)
-			returnValue.Set("showAsInline", wasmQueryShowAsInline)
-			returnValue.Set("showAsInlineBlock", wasmQueryShowAsInlineBlock)
-			returnValue.Set("showAsFlex", wasmQueryShowAsFlex)
-			returnValue.Set("val", wasmQueryVal)
-			return returnValue
-		} else if string(domSelector[0]) == "." {
-			returnValue := js.Global().Get("document").Call("getElementsByClassName", selectorWithoutSymbol)
-			for i := 0; i < returnValue.Length(); i++ {
-				returnValue.Index(i).Set("css", wasmQueryCSS)
-				returnValue.Index(i).Set("hide", wasmQueryHide)
-				returnValue.Index(i).Set("show", wasmQueryShow)
-				returnValue.Index(i).Set("showAsBlock", wasmQueryShowAsBlock)
-				returnValue.Index(i).Set("showAsInline", wasmQueryShowAsInline)
-				returnValue.Index(i).Set("showAsInlineBlock", wasmQueryShowAsInlineBlock)
-				returnValue.Index(i).Set("showAsFlex", wasmQueryShowAsFlex)
-				returnValue.Index(i).Set("val", wasmQueryVal)
-			}
-			returnValue.Set("css", wasmQueryCSSForArray)
-			returnValue.Set("hide", wasmQueryHideForArray)
-			returnValue.Set("show", wasmQueryShowForArray)
-			returnValue.Set("showAsBlock", wasmQueryShowAsBlockForArray)
-			returnValue.Set("showAsInline", wasmQueryShowAsInlineForArray)
-			returnValue.Set("showAsInlineBlock", wasmQueryShowAsInlineBlockForArray)
-			returnValue.Set("showAsFlex", wasmQueryShowAsFlexForArray)
-			returnValue.Set("val", wasmQueryValForArray)
-			return returnValue
-		}
-	} else {
-		returnValue := js.Global().Get("document").Call("getElementsByTagName", domSelector)
-		for i := 0; i < returnValue.Length(); i++ {
-			returnValue.Index(i).Set("css", wasmQueryCSS)
-			returnValue.Index(i).Set("hide", wasmQueryHide)
-			returnValue.Index(i).Set("show", wasmQueryShow)
-			returnValue.Index(i).Set("showAsBlock", wasmQueryShowAsBlock)
-			returnValue.Index(i).Set("showAsInline", wasmQueryShowAsInline)
-			returnValue.Index(i).Set("showAsInlineBlock", wasmQueryShowAsInlineBlock)
-			returnValue.Index(i).Set("showAsFlex", wasmQueryShowAsFlex)
-			returnValue.Index(i).Set("val", wasmQueryVal)
-		}
-		returnValue.Set("css", wasmQueryCSSForArray)
-		returnValue.Set("hide", wasmQueryHideForArray)
-		returnValue.Set("show", wasmQueryShowForArray)
-		returnValue.Set("showAsBlock", wasmQueryShowAsBlockForArray)
-		returnValue.Set("showAsInline", wasmQueryShowAsInlineForArray)
-		returnValue.Set("showAsInlineBlock", wasmQueryShowAsInlineBlockForArray)
-		returnValue.Set("showAsFlex", wasmQueryShowAsFlexForArray)
-		returnValue.Set("val", wasmQueryValForArray)
-		return returnValue
+	domSelector := args[0].String()
+	returnValue := js.Global().Get("document").Call("querySelectorAll", domSelector)
+	for i := 0; i < returnValue.Length(); i++ {
+		returnValue.Index(i).Set("css", wasmQueryCSS)
+		returnValue.Index(i).Set("hide", wasmQueryHide)
+		returnValue.Index(i).Set("show", wasmQueryShow)
+		returnValue.Index(i).Set("showAsBlock", wasmQueryShowAsBlock)
+		returnValue.Index(i).Set("showAsInline", wasmQueryShowAsInline)
+		returnValue.Index(i).Set("showAsInlineBlock", wasmQueryShowAsInlineBlock)
+		returnValue.Index(i).Set("showAsFlex", wasmQueryShowAsFlex)
+		returnValue.Index(i).Set("val", wasmQueryVal)
 	}
-	return nil
+	returnValue.Set("css", wasmQueryCSSForArray)
+	returnValue.Set("hide", wasmQueryHideForArray)
+	returnValue.Set("show", wasmQueryShowForArray)
+	returnValue.Set("showAsBlock", wasmQueryShowAsBlockForArray)
+	returnValue.Set("showAsInline", wasmQueryShowAsInlineForArray)
+	returnValue.Set("showAsInlineBlock", wasmQueryShowAsInlineBlockForArray)
+	returnValue.Set("showAsFlex", wasmQueryShowAsFlexForArray)
+	returnValue.Set("val", wasmQueryValForArray)
+	return returnValue
 })
--- src/wasmQueryCSSForArray.go
+++ src/wasmQueryCSSForArray.go
@@ -15,11 +15,19 @@ var wasmQueryCSSForArray js.Func = js.FuncOf(func(this js.Value, args []js.Value
 		}
 		return outputArray
 	} else if len(args) >= 2 {
-		var value string = args[1].String()
-		for i := 0; i < this.Length(); i++ {
-			this.Index(i).Get("style").Set(selector, value)
-			valueOfIteration := this.Index(i).Get("style").Get(selector)
-			outputArray = append(outputArray, valueOfIteration)
+		value := args[1]
+		if value.Type().String() == "object" {
+			for i := 0; i < this.Length(); i++ {
+				this.Index(i).Get("style").Set(selector, value.Index(i).String())
+				valueOfIteration := this.Index(i).Get("style").Get(selector)
+				outputArray = append(outputArray, valueOfIteration)
+			}
+		} else {
+			for i := 0; i < this.Length(); i++ {
+				this.Index(i).Get("style").Set(selector, value.String())
+				valueOfIteration := this.Index(i).Get("style").Get(selector)
+				outputArray = append(outputArray, valueOfIteration)
+			}
 		}
 		return outputArray
 	}
